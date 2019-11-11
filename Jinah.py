f3e2fb81d6c40f2c96968c106bbac65793a06fe8
Jinah <jinah2001@sasa.hs.kr>
2019-11-11 15:08:58 +0900

    02update Jinah.py

--- Jinah.py
+++ Jinah.py
@@ -1,2 +1,16 @@
-print("Hello World")
-print("update")
+#제출란 찾기
+import bs4
+import requests
+
+def get_html(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.txt
+
+html = get_html("https://go.sasa.hs.kr/board/lists/190205/page/1")
+soup = bs4.BeautifulSoup(html, 'html.parser')
+
+search_name = soup.select()
+
+for i in search_name:
