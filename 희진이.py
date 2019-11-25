f7e93b584da1ca6155f0d6c804026559cefb4a82
Jinah <jinah2001@sasa.hs.kr>
2019-11-25 14:58:07 +0900

    08update Jinah.py

--- 희진이.py
+++ 희진이.py
@@ -1,1 +1,11 @@
-print("희진이")
+import datetime
+
+class day_cal:
+    def __init__(self):
+        self.day = datetime.datetime.now()
+
+    def calculator(self, end_day):
+        dt_today = self.day
+        dt_end = datetime.datetime(int(end_day.split('-')[0]), int(end_day.split('-')[1]), int(end_day.split('-')[2]))
+        td = dt_end - dt_today
+        return td.days
